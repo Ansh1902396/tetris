c1ea392fd29b2bc2f8c6d29e8e6df48fc2f89ff5
rudransh <rudransh9shinghal@gmail.com>
2024-06-10 20:14:15 +0530

    done with termentino meet

--- src/main.rs
+++ src/main.rs
@@ -251,6 +251,56 @@ fn create_new_tetrimino() -> Tetrimino {
 }
 
 
+impl Tetrimino {
+    fn rotate (&mut self , game_map : &[Vec<u8>]) {
+        
+        let mut tmp_state = self.current_state + 1 ; 
+
+        if tmp_state as usize >= self.states.len() { 
+            tmp_state = 0 ; 
+        }
+
+        let x_pos = [0 , -1 , 1 , -2 , 2 , -3]; 
+
+        for x in x_pos.iter(){  
+            if self.test_position(game_map, tmp_state as usize, self.x + x, self.y ) == true { 
+                self.current_state = tmp_state ; 
+                self.x += *x ; 
+                break;
+            }
+        }
+    }
+
+    fn test_position(&self , game_map : &[Vec<u8>] , temp_state :usize ,x :isize , y:usize) -> bool { 
+        for decal_y in 0..4 {
+            for decal_x in 0..4 {
+                let x = x + decal_x ; 
+                
+                if self.states[temp_state][decal_y][decal_x as usize] != 0 && (
+                    y + decal_y >= game_map.len() || 
+                    x < 0 || 
+                    x as usize > game_map[y + decal_y].len() || 
+                    game_map[y + decal_y][x as usize] != 0
+                ) {
+                    return  false;
+                }
+            }
+        }
+        return  true;
+    }
+
+    fn change_position (&mut self , game_map : &[Vec<u8>] , new_x  : isize , new_y  :usize) -> bool { 
+        if self.test_position(game_map , self.current_state as usize , new_x , new_y) == true {
+            self.x == new_x as isize ; 
+            self.y = new_y ;
+            true
+        } else  { 
+            false
+        }
+    }
+}
+
+
 fn create_texture_rect<'a>(
     canvas: &mut Canvas<Window>,
     texture_creator: &'a TextureCreator<WindowContext>,
